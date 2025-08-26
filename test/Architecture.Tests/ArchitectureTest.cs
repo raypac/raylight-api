0513b932abb328b1a3c401c4bd4ae42f5e322ede
Ray Pacayra <raymond.pacayra@gmail.com>
2025-08-26 08:45:50 +0800

    Add Unit Testing and Update Github Workflow
    - Add Architecture and Domain Unit Testing
    - Update Github Workflow

---
+++ test/Architecture.Tests/ArchitectureTest.cs
@@ -0,0 +1,147 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Architecture.Test;
+
+public class ArchitectureTest
+{
+    private const string DomainNamespace = "RaylightApi.Domain";
+    private const string ApplicationNamespace = "RaylightApi.Application";
+    private const string InfrastructureNamespace = "RaylightApi.Infrastructure";
+    private const string PresentationNamespace = "RaylightApi.Presentation";
+    private const string WebApiNamespace = "RaylightApi.WebApi";
+
+    [Fact]
+    public void Domain_Should_Not_Have_DepedencyOnOtherProject()
+    {
+        // Arrange
+        var assembly = RaylightApi.Domain.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAll(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_Have_DepedencyOnOtherProject()
+    {
+        // Arrange
+        var assembly = RaylightApi.Application.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAll(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Handlers_Should_Not_Have_DepedencyOnDomain()
+    {
+        // Arrange
+        var assembly = RaylightApi.Application.AssemblyReference.Assembly;
+
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .That()
+                          .HaveNameEndingWith("Handler")
+                          .Should()
+                          .HaveDependencyOn(DomainNamespace)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Not_Have_DepedencyOnOtherProject()
+    {
+        // Arrange
+        var assembly = RaylightApi.Infrastructure.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAll(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_Have_DepedencyOnOtherProject()
+    {
+        // Arrange
+        var assembly = RaylightApi.Presentation.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            WebApiNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .ShouldNot()
+                          .HaveDependencyOnAll(otherProjects)
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Controllers_Should_Not_Have_DepedencyOnMediatR()
+    {
+        // Arrange
+        var assembly = RaylightApi.Infrastructure.AssemblyReference.Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            WebApiNamespace
+        };
+
+        // Act
+        var result = Types.InAssembly(assembly)
+                          .That()
+                          .HaveNameEndingWith("Controller")
+                          .Should()
+                          .HaveDependencyOn("MediatR")
+                          .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
---
+++ test/Domain.Tests/Entities/MemberTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using RaylightApi.Domain.Entities;
+using RaylightApi.Domain.ValueObjects;
+
+namespace Domain.Tests.Entities;
+
+public class MemberTests
+{
+    [Fact]
+    public void Create_GivenValidReference_ShouldReturnMember()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var userName = "test";
+        var email = Email.Create("test@email.com").Value;
+        var emailConfirmed = true;
+        var phoneNumber = "1234567890";
+        var phoneNumberConfirmed = true;
+        var twoFactorEnabled = true;
+        var lockoutEnd = DateTime.UtcNow;
+        var lockoutEnabled = false;
+        var accessFailedCount = 10;
+
+        // Act
+        var member = Member.Create(userName, 
+                                     email, 
+                                     emailConfirmed, 
+                                     phoneNumber, 
+                                     phoneNumberConfirmed, 
+                                     twoFactorEnabled, 
+                                     lockoutEnd, 
+                                     lockoutEnabled, 
+                                     accessFailedCount, 
+                                     id);
+
+        // Assert
+        member.UserName.Should().Be(userName);
+        member.Email.Should().Be(email);
+        member.EmailConfirmed.Should().Be(emailConfirmed);
+        member.PhoneNumber.Should().Be(phoneNumber);
+        member.PhoneNumberConfirmed.Should().Be(phoneNumberConfirmed);
+        member.TwoFactorEnabled.Should().Be(twoFactorEnabled);
+        member.LockoutEnd.Should().Be(lockoutEnd);
+        member.LockoutEnabled.Should().Be(lockoutEnabled);
+        member.AccessFailedCount.Should().Be(accessFailedCount);
+        member.Id.Should().Be(id);
+    }
+}
